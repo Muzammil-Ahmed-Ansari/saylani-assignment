e46c812b97e6f3dd1d1130e5334417ddca31f8fa
Muzammil Ahmed Ansari <140959896+Muzammil-Ahmed-Ansari@users.noreply.github.com>
2023-09-29 10:43:54 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+                                          //! ASSIGNMENT
+
+//  Q1 const multiDimensionalArray = [];
+
+//  Q2  var a = const matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+//   ];
+//   document.write(a)
+
+//  Q3 for (let i = 1; i <= 10; i++) {
+//   document.write(i);
+//   }
+  
+//  Q4 // Prompt the user for the table number and length
+// const tableNumber = parseInt(prompt("Enter the table number:"));
+// const tableLength = parseInt(prompt("Enter the table length:"));
+
+// // Check if the input is a valid number
+// if (isNaN(tableNumber) || isNaN(tableLength)) {
+//   console.log("Please enter valid numbers.");
+// } else {
+//   // Print the multiplication table
+//   console.log(`Multiplication Table for ${tableNumber} (up to ${tableLength}):`);
+//   for (let i = 1; i <= tableLength; i++) {
+//     const result = tableNumber * i;
+//     console.log(`${tableNumber} x ${i} = ${result}`);
+//   }
+// }
+//  
+
+// Q6
+// // // Define the array
+// const A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// // Prompt the user for input
+// const userInput = prompt("Enter an item to search:");
+
+// // Use the includes() method to check if the item is in the array
+// if (A.includes(userInput)) {
+//   console.log(`Yes, '${userInput}' is found in the list.`);
+// } else {
+//   console.log(`No, '${userInput}' is not found in the list.`);
+// }
+
+// Q7
+// Define the array
+// const A = [24, 53, 78, 91, 12];
+
+// // Initialize a variable to store the maximum value
+// let max = A[0]; // Assuming the first element is the maximum initially
+
+// // Iterate through the array
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] > max) {
+//     max = A[i]; // Update the maximum value if a larger value is found
+//   }
+// }
+
+// // Print the maximum value
+// document.write(`The largest number in the array is: ${max}`);
+
+// Q9
+// Define the array
+// const A = [24, 53, 78, 91, 12];
+
+// // Initialize a variable to store the minimum value
+// let min = A[0]; // Assuming the first element is the minimum initially
+
+// // Iterate through the array
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] < min) {
+//     min = A[i]; // Update the minimum value if a smaller value is found
+//   }
+// }
+
+// // Print the minimum value
+// console.log(`The smallest number in the array is: ${min}`);
+// Q10
+// document.write("Multiples of 5 from 1 to 100:");
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//    document.write(i);
+//   }
+// }
